2de9015032b762e3e7a6d8f5d48bf1f48d43b1b0
Kvkens <kvkens@qq.com>
2015-01-24 15:22:12 +0800

    修正后台登陆地址

--- fullstack/footer.php
+++ fullstack/footer.php
@@ -1,6 +1,6 @@
 		<div id="footer">
 			<p><?php echo get_option('footerText'); ?></p>
-			<p> <a href="http://www.miitbeian.gov.cn/" target="_blank" style="color:#f9faf5">京ICP备15000098号</a> CopyRight <a href="wp-login.php">&copy;</a> 2001-<?php echo date("Y"); ?>&nbsp;Power&nbsp;By&nbsp;<a href="http://www.imyy.org/" target="_blank">Kvkens</a>
+			<p> <a href="http://www.miitbeian.gov.cn/" target="_blank" style="color:#f9faf5">京ICP备15000098号</a> CopyRight <a href="../wp-login.php">&copy;</a> 2001-<?php echo date("Y"); ?>&nbsp;Power&nbsp;By&nbsp;<a href="http://www.imyy.org/" target="_blank">Kvkens</a>
 			 
 			</p>
 		</div>
